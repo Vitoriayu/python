401f5e7c7498ba39b7606c2450503868553ae194
VITORIA GABRIELA DOS SANTOS <vitoria.8990@aluno.pr.senac.br>
2023-11-07 14:01:31 -0300

    kk

--- ex05.py
+++ ex05.py
@@ -1,2 +1,10 @@
-opcao = int(input('escolha uma opção:\n1 - Maça\n2 - Banana\n3 - Laranja\nOpção:'))
+opcao = int(input('escolha uma opcao: \n1 - maçã\n2 - banana\n3 laranja\nOpção '))
 
+if opcao == 1:
+    print ('voce escolheu Maçã.')
+elif opcao == 2:
+    print('voce escolheu banana.')
+elif opcao == 3:
+    print('voce esolheu laranja')
+else:
+    print('opção inválida')
