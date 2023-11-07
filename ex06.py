59285a61b06f8a66161514eedff5bc4df70d43ed
VITORIA GABRIELA DOS SANTOS <vitoria.8990@aluno.pr.senac.br>
2023-11-07 14:26:26 -0300

    kk

--- ex06.py
+++ ex06.py
@@ -3,3 +3,6 @@ contador = 1
 while contador <=5:
     print('Contador:', contador)
     contador = contador + 1
+
+    for contador in range(1,6):
+        print('contador:', contador)
---
+++ ex08.py
@@ -0,0 +1,6 @@
+lista1 = ['vitoria','gabriela','santos','fer',]
+print (type(lista1))
+
+print(len(lista1))
+
+print(lista1[4])
